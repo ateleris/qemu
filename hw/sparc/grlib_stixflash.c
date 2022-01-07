0cb61689af8568c71f733631d4766876167e8d20
Laszlo Etesi <laszlo.etesi@ateleris.ch>
2022-01-07 17:20:30 +0100

    fixed write addresses and masks

--- hw/sparc/grlib_stixflash.c
+++ hw/sparc/grlib_stixflash.c
@@ -20,12 +20,15 @@
 
 /* DEFINES TAKEN FROM stixidpu.h */
 #define FTNANDCTRL_PAGEADDRESS_CHIPADDRESS 20
+#define FTNANDCTRL_PAGEADDRESS_CHIPADDRESS_MASK 0x7
 #define FTNANDCTRL_PAGEADDRESS_PAGEADDRESS 0
+#define FTNANDCTRL_PAGEADDRESS_PAGEADDRESS_MASK 0x3FFFF
 #define FTNANDCTRL_CMDSTA_COMMAND 0
 #define FTNANDCTRL_CMDSTA_COMMAND_MASK 0x7
 #define FTNANDCTRL_CMDSTA_MCMADDRESS 3
 #define FTNANDCTRL_CMDSTA_MCMADDRESS_MASK 0x1
 #define FTNANDCTRL_CMDSTA_FLASHTYPE 7
+#define FTNANDCTRL_CMDSTA_FLASHTYPE_MASK 0x1
 #define FTNANDCTRL_CMDSTA_BUSY 4
 #define FTNANDCTRL_CMDSTA_STATUS 24
 
@@ -52,7 +55,7 @@ struct STIXFLASH
     SysBusDevice parent_obj;
 
     FILE *f;
-    uint8_t pageData[FLASH_PAGE_SIZE];
+    uint8_t page_data[FLASH_PAGE_SIZE];
 
     MemoryRegion iomem;
     qemu_irq irq;
@@ -60,6 +63,8 @@ struct STIXFLASH
     uint32_t ram_address;
     uint32_t page_address;
     uint32_t cmd_sta;
+
+    uint32_t write_counter; // flash commanding is configured with three writes, this counter keeps track of that
 
     uint32_t chip;
     uint32_t page;
@@ -80,17 +85,62 @@ struct STIXFLASH
 
 */
 
-static uint64_t grlib_stixflash_read(void *opaque, hwaddr addr,
-                                     unsigned size)
+/*
+    This function resets the flash.
+
+    The reset command, at this point, always successfully completes and does not use
+    any of the flash targets (page addres, chip, etc.)
+*/
+static void execute_flash_reset(STIXFLASH *stixflash)
+{
+    // with the following register update, we signal that the flash successfully reset
+    stixflash->cmd_sta &= ~(1u << FTNANDCTRL_CMDSTA_BUSY);
+    
+    // make sure we tell the software that the read was good.
+    // NB: it also works without this, but I assume we should set it to ready
+    // let's see if we need it
+    // stixflash->cmd_sta |= (1u << ERROR_CODE_READY);
+}
+
+static void execute_flash_read(STIXFLASH *stixflash)
+{
+    int32_t ret = 0;
+
+    fseek(stixflash->f, 0, SEEK_END);
+    int64_t length = ftell(stixflash->f);
+
+    // TODO: check if page_address is unique or if we do neet to include chip, page and mcm
+    uint32_t position_in_file = stixflash->page_address * FLASH_PAGE_SIZE;
+
+    if(length < (position_in_file + FLASH_PAGE_SIZE + 1))
+    {
+        assert(false);
+    }
+
+    fseek(stixflash->f, position_in_file, SEEK_SET);
+    ret = fread(stixflash->page_data, sizeof(uint8_t), FLASH_PAGE_SIZE, stixflash->f);
+    assert(ret);
+    fseek(stixflash->f, 0, SEEK_END);
+
+    // update the flash register to be not busy anymore
+    stixflash->cmd_sta &= ~(1u << FTNANDCTRL_CMDSTA_BUSY);
+
+    // need to send out an interrupt that we're done
+    qemu_irq_pulse(stixflash->irq); //, 1); // NB: needs to be checked if '1' is OK in all cases. There is a comment that for LEON level needs to be equal to IRQ number. 
+}
+
+static uint64_t grlib_stixflash_read(void *opaque, hwaddr addr, unsigned size)
 {
     STIXFLASH *stixflash = opaque;
 
     qemu_printf("Read requested: addr = %lu, chip = %u, page = %u, mcm = %u, page_address = %u, memory_bit_type = %u, ram_address = 0x%x, command = %u, status = 0x%x\n",
                 addr, stixflash->chip, stixflash->page, stixflash->mcm, stixflash->page_address, stixflash->memory_bit_type, stixflash->ram_address, stixflash->command, stixflash->cmd_sta);
 
-    if (addr == FLASH_CMD_STA_ADDRESS)
+    assert(stixflash->write_counter == 0);
+
+    if (addr == FLASH_CMD_STA_ADDRESS) // this is a read of the flash status register
     {
-        return stixflash->cmd_sta & ~(1u << FTNANDCTRL_CMDSTA_BUSY); //| (1u << FTNANDCTRL_CMDSTA_BUSY); // add this to fake a busy flash status
+        return stixflash->cmd_sta;
     }
     else if (addr == FLASH_PAGE_ADDRESS)
     {
@@ -106,72 +156,73 @@ static uint64_t grlib_stixflash_read(void *opaque, hwaddr addr,
     }
 }
 
-static void grlib_stixflash_write(void *opaque, hwaddr addr,
-                                  uint64_t value, unsigned size)
+static void grlib_stixflash_write(void *opaque, hwaddr addr, uint64_t value, unsigned size)
 {
     STIXFLASH *stixflash = opaque;
 
-    if (addr == FLASH_CMD_STA_ADDRESS)
+    switch (addr)
     {
+    case FLASH_CMD_STA_ADDRESS: // update the status register
+        assert(stixflash->write_counter == 2);
         stixflash->cmd_sta = (uint32_t)value;
         stixflash->command = (stixflash->cmd_sta >> FTNANDCTRL_CMDSTA_COMMAND) & FTNANDCTRL_CMDSTA_COMMAND_MASK;
         stixflash->mcm = (stixflash->cmd_sta >> FTNANDCTRL_CMDSTA_MCMADDRESS) & FTNANDCTRL_CMDSTA_MCMADDRESS_MASK;
-    }
-    else if (addr == FLASH_PAGE_ADDRESS)
-    {
+        stixflash->memory_bit_type = (stixflash->cmd_sta >> FTNANDCTRL_CMDSTA_FLASHTYPE) & FTNANDCTRL_CMDSTA_FLASHTYPE_MASK;
+        stixflash->write_counter += 1;
+        break;
+    
+    case FLASH_PAGE_ADDRESS: // configure the flash addressing
+        assert(stixflash->write_counter == 1);
         stixflash->page_address = (uint32_t)value;
-        stixflash->chip = (stixflash->page_address >> FTNANDCTRL_CMDSTA_COMMAND) & 0x7;
-        stixflash->page = (stixflash->page_address >> FTNANDCTRL_PAGEADDRESS_PAGEADDRESS) & 0x3FFFF;
-        stixflash->memory_bit_type = (stixflash->page_address) & 0x1;
-    }
-    else if (addr == FLASH_RAM_ADDRESS)
-    {
+        stixflash->chip = (stixflash->page_address >> FTNANDCTRL_PAGEADDRESS_CHIPADDRESS) & FTNANDCTRL_PAGEADDRESS_CHIPADDRESS_MASK;
+        stixflash->page = (stixflash->page_address >> FTNANDCTRL_PAGEADDRESS_PAGEADDRESS) & FTNANDCTRL_PAGEADDRESS_PAGEADDRESS_MASK;
+        stixflash->write_counter += 1;
+        break;
+
+    case FLASH_RAM_ADDRESS: // configure the ram address (write-back?)
+        assert(stixflash->write_counter == 0);
         stixflash->ram_address = (uint32_t)value;
-    }
-    else
-    {
+        stixflash->write_counter += 1;
+        break;
+    default:
         assert(false);
     }
 
     // Separating the last write command out on purpose for legibility
-    if (addr == FLASH_CMD_STA_ADDRESS)
+    if (stixflash->write_counter == 3)
     {
+        // mock the flash and fpga readiness a bit; while executing the command
+        // we are not ready; at the end of this routine, we will be again
+        stixflash->cmd_sta &= ~(1u << ERROR_CODE_READY);
+
         // now we execute the flash command
         switch (stixflash->command)
         {
         case COMMAND_RESET:
-            // with the following register update, we signal that the flash successfully reset
-            stixflash->cmd_sta &= ~(1u << FTNANDCTRL_CMDSTA_BUSY);
-            
-            // make sure we tell the software that the read was good.
-            // NB: it also works without this, but I assume we should set it to ready
-            // let's see if we need it
-            // stixflash->cmd_sta |= (1u << ERROR_CODE_READY);
+            execute_flash_reset(stixflash);
+            break;
+        
+        case COMMAND_PAGE_READ:
+            execute_flash_read(stixflash);
             break;
         
         default:
+            qemu_printf("UNKNOWN COMMAND %u\n", stixflash->command);
             assert(false);
         }
 
-
-        // make sure we are not busy anymore when we send out the ready read interrupt
-        //stixflash->cmd_sta &= ~(1u << FTNANDCTRL_CMDSTA_BUSY);
-
         // make sure we tell the software that the read was good.
-        //stixflash->cmd_sta |= (1u << ERROR_CODE_READY);
+        stixflash->cmd_sta |= (1u << ERROR_CODE_READY);
 
         // qemu_printf("Flash request fully configured\n");
         qemu_printf("Write completed: chip = %u, page = %u, mcm = %u, page_address = %u, memory_bit_type = %u, ram_address = 0x%x command = %u, status = 0x%x\n",
                     stixflash->chip, stixflash->page, stixflash->mcm, stixflash->page_address, stixflash->memory_bit_type, stixflash->ram_address, stixflash->command, stixflash->cmd_sta);
 
-        if (stixflash->command == COMMAND_PAGE_READ)
-        {
-            qemu_irq_pulse(stixflash->irq); //, 1); // NB: needs to be checked if '1' is OK in all cases. There is a comment that for LEON level needs to be equal to IRQ number.
-        }
+        // reset the write counter to 0
+        stixflash->write_counter = 0;
     }
 
-    // qemu_printf("WRITE: %lu - %lu - %u\n", addr, value, size);
-    // qemu_printf_grlib_apbuart_readl_unknown(addr);
+    assert(stixflash->write_counter < 4);
 }
 
 static const MemoryRegionOps grlib_stixflash_ops = {
@@ -201,12 +252,12 @@ static void init_and_load_flash_file(STIXFLASH *stixflash)
             exit(1);
         }
 
-        memset(stixflash->pageData, 0xFF, FLASH_PAGE_SIZE);
+        memset(stixflash->page_data, 0xFF, FLASH_PAGE_SIZE);
 
         fseek(stixflash->f, 0, SEEK_SET);
         for (uint32_t i = 0; i < 32 * 1024; i++)
         {
-            fwrite(stixflash->pageData, sizeof(stixflash->pageData[0]), sizeof(stixflash->pageData) / sizeof(stixflash->pageData[0]), stixflash->f);
+            fwrite(stixflash->page_data, sizeof(stixflash->page_data[0]), sizeof(stixflash->page_data) / sizeof(stixflash->page_data[0]), stixflash->f);
         }
         qemu_printf("file has been created");
         if (0 != fclose(stixflash->f))
